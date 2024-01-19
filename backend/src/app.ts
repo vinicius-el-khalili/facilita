d60f999561ba2ca3099b5653a3e10108f142944f
vini <velkhaliliborsato@gmail.com>
2024-01-19 00:55:44 -0300

    checkpoint

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express = require("express");
 import path = require("path");
-import bodyParser = require("body-parser");
+import bodyParser = require("body-parser")
 import consolidate = require("consolidate")
 import dust = require("dustjs-helpers")
 import pg = require ("pg")
+
