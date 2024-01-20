f85e361982484a0c2d8e9b57cb4e341101cdf6e2
vini <velkhaliliborsato@gmail.com>
2024-01-20 00:54:51 -0300

    sass

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './App.scss'
+import style from "./test.module.scss"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <div className={style.test}>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
