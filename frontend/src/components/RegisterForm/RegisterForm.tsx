fac463cf0dbd3525562d6ad787358d23cffe668d
vini <velkhaliliborsato@gmail.com>
2024-01-22 18:20:30 -0300

    RegisterFormContainer

---
+++ frontend/src/components/RegisterForm/RegisterForm.tsx
@@ -0,0 +1,10 @@
+import style from "./RegisterForm.module.scss"
+const RegisterForm = () => {
+    return (
+        <div className={style.RegisterForm}>
+            RegisterForm
+        </div>
+    );
+}
+ 
+export default RegisterForm;
--- frontend/src/components/Toolbar/Toolbar.tsx
+++ frontend/src/components/Toolbar/Toolbar.tsx
@@ -1,15 +1,30 @@
 import { FaPlus } from 'react-icons/fa';
 import SearchBox from '../SearchBox/SearchBox';
 import style from './Toolbar.module.scss'
+import { useState } from 'react';
+import RegisterForm from '../RegisterForm/RegisterForm';
 const Toolbar = () => {
+
+    const [registerForm,setRegisterForm] = useState<boolean>(false)
+
     return (
         <div className={style.Toolbar}>
+
             <SearchBox/>
+
             <div className={style.registerButton}>
-                <div className={style.button}>
+                <div
+                className={style.button}
+                onClick={()=>{setRegisterForm(!registerForm)}}
+                >
                     <FaPlus/> Cadastrar cliente
                 </div>
             </div>
+
+            <div className={`${style.RegisterFormContainer} ${registerForm&&style.RegisterFormContainerOn}`}>
+                <RegisterForm/>
+            </div>
+
         </div>
     );
 }
