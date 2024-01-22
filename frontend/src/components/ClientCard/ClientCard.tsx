77efc9324bbaa61e4283a12c714520c1e7eb22dd
vini <velkhaliliborsato@gmail.com>
2024-01-22 17:10:57 -0300

    ClientCard delete button

--- frontend/src/components/ClientCard/ClientCard.tsx
+++ frontend/src/components/ClientCard/ClientCard.tsx
@@ -3,8 +3,15 @@ import style from "./ClientCard.module.scss"
 import { MdOutlineMail } from "react-icons/md";
 import { LuMapPin, LuPhone } from "react-icons/lu";
 import { FaRegTrashCan } from "react-icons/fa6";
+import { useState } from "react";
 
 const ClientCard = () => {
+
+    const [deleteCheck,setDeleteCheck] = useState<boolean>(true)
+    const handleDeleteButton = () => {
+        setDeleteCheck(!deleteCheck)
+    }
+
     return (
         <div className={style.ClientCard}>
             <div className={style.attributes}>
@@ -30,9 +37,23 @@ const ClientCard = () => {
                 </div>
 
             </div>
-            <div className={style.deleteButton}>
+            <div
+            className={style.deleteButton}
+            onClick={handleDeleteButton}
+            style={deleteCheck?{backgroundColor:"var(--red)"}:{}}
+            >
                 <FaRegTrashCan/>
             </div>
+
+            {deleteCheck &&
+            <div className={style.deleteCheckBox}>
+                <p>Deletar usuário?</p>
+                <div className={style.buttons}>
+                    <div className={style.button}>Sim</div>
+                    <div className={style.button}>Não</div>
+                </div>
+            </div>
+            }
 
         </div>
     );
